4b7e4976ec33c3e84c5f06c5beb66275f2b332ea
Ashatta <tatjana.agapova@gmail.com>
2013-03-12 00:09:15 +0400

    hw3.task1

--- semester_4/hw_03/t_1/hw3.task1.hs
+++ semester_4/hw_03/t_1/hw3.task1.hs
@@ -3,6 +3,7 @@ import Test.QuickCheck
 func x l = map (\y -> y*x) l
 -- func x = map (\y -> y*x)
 -- func x = map (\y -> (*x) y)
-func2 x = map (*x)
+-- func x = map (*x)
+func2 =  map . (*)
 
 main = quickCheck ((\x l -> func x l == func2 x l) :: Int -> [Int] -> Bool)
