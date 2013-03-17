2e0fb96471cc42263fd04e75304048c471dca136
Ashatta <tatjana.agapova@gmail.com>
2013-03-18 01:17:53 +0400

    another solution added

--- semester_4/hw_05/t_2/hw5.task2.hs
+++ semester_4/hw_05/t_2/hw5.task2.hs
@@ -6,4 +6,9 @@ check1 pred = foldr ((&&) . pred) True
 check2 :: (a -> Bool) -> [a] -> Bool
 check2 pred xs = null [ x | x <- xs, not (pred x) ]
 
-main = quickCheck ((\xs -> check1 (>0) xs == check2 (>0) xs) :: [Int] -> Bool)
+check3 :: (a -> Bool) -> [a] -> Bool
+check3 pred = null . filter (not . pred)
+
+main = do
+    quickCheck ((\xs -> check1 (>0) xs == check2 (>0) xs) :: [Int] -> Bool)
+    quickCheck ((\xs -> check3 (>0) xs == check2 (>0) xs) :: [Int] -> Bool)
