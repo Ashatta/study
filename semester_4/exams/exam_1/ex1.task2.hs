c32fdb468d87e30f981243f98727ab8023f86cdd
Ashatta <tatjana.agapova@gmail.com>
2016-06-08 17:27:00 +0300

    blah blah

--- semester_4/exams/exam_1/ex1.task2.hs
+++ semester_4/exams/exam_1/ex1.task2.hs
@@ -1,3 +1,6 @@
+import qualified Data.Foldable as F
+import Data.Monoid
+
 data Tree a = Nil | Node a (Tree a) (Tree a)
       deriving (Eq, Show)
 
@@ -8,3 +11,10 @@ flatten = helper []
 
 negativeElements :: (Num a, Ord a) => Tree a -> [a]
 negativeElements = filter (<0) . flatten
+
+instance F.Foldable Tree where
+    foldMap f Nil = mempty
+    foldMap f (Node x left right) = F.foldMap f left `mappend` f x `mappend` F.foldMap f right
+
+flatten' :: Tree a -> [a]
+flatten' = F.foldMap (\x -> [x])
